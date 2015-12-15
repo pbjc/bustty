f8b0cb698d26643c6be7aa15ea1c6fa6194802f2
Paul Choi <choipaul96@gmail.com>
2015-12-15 13:14:11 -0600

    reorder defs

--- bustty.py
+++ bustty.py
@@ -4,17 +4,23 @@ import re
 import urllib
 import xml.etree.ElementTree as ElementTree
 
-class Departure:
-    def __init__(self, sign, time, minutes):
-        self.sign = sign
-        self.time = time
-        self.minutes = minutes
+def main():
+    parser = argparse.ArgumentParser(
+        description='Display Capital Metro bus stop departures')
+    parser.add_argument('stop', type=int, help='the bus stop id')
+    parser.add_argument('route', type=int, nargs='?',
+        help='the bus route number')
+    parser.add_argument('--n', metavar='N', dest='num_results', type=int,
+        default=3, help='the number of departures to display (default: 3)')
+    args = parser.parse_args()
 
-    def __str__(self):
-        if self.minutes:
-            return '{} {} min'.format(self.sign, self.minutes)
-        else:
-            return '{} {}'.format(self.sign, self.time)
+    stop = Stop(args.stop, args.route, args.num_results)
+    try:
+        stop.update()
+    except InvalidStopIdException:
+        print 'Invalid stop id:', args.stop
+        exit(1)
+    print stop
 
 class Stop:
     def __init__(self, stop, route=None, num_results=3):
@@ -59,26 +65,20 @@ class Stop:
             lines.append(str(departure))
         return '\n'.join(lines)
 
+class Departure:
+    def __init__(self, sign, time, minutes):
+        self.sign = sign
+        self.time = time
+        self.minutes = minutes
+
+    def __str__(self):
+        if self.minutes:
+            return '{} {} min'.format(self.sign, self.minutes)
+        else:
+            return '{} {}'.format(self.sign, self.time)
+
 class InvalidStopIdException(Exception):
     pass
-
-def main():
-    parser = argparse.ArgumentParser(
-        description='Display Capital Metro bus stop departures')
-    parser.add_argument('stop', type=int, help='the bus stop id')
-    parser.add_argument('route', type=int, nargs='?',
-        help='the bus route number')
-    parser.add_argument('--n', metavar='N', dest='num_results', type=int,
-        default=3, help='the number of departures to display (default: 3)')
-    args = parser.parse_args()
-
-    stop = Stop(args.stop, args.route, args.num_results)
-    try:
-        stop.update()
-    except InvalidStopIdException:
-        print 'Invalid stop id:', args.stop
-        exit(1)
-    print stop
 
 if __name__ == '__main__':
     main()
